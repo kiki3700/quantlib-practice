c25e0cad2f4e9acfc84f7df75f858492576bafde
kiki3700 <5575492q@gmail.com>
2021-07-23 16:17:37 +0900

    bond pricing

---
+++ week2-bonds-pricing/UST_PRICING.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 23 14:58:53 2021
+
+@author: ben
+"""
+
+import QuantLib as ql
+from UST_CURVE import GET_DATE, GET_QUOTE, TREASURY_CURVE
+
+# Basic setup
+eval_date = GET_DATE()
+quote = GET_QUOTE(eval_date)
+curve = TREASURY_CURVE(eval_date, quote)
+
+# handl , engine
+curveHandle = ql.YieldTermStructureHandle(curve)
+bondEngine = ql.DiscountingBondEngine(curveHandle)
+
+# Treasury bond information
+issueDate =ql.Date(13,11,2020)
+maturityDate =ql.Date(13,11,2030)
+tenor = ql.Period(ql.Semiannual)
+calendar = ql.UnitedStates()
+convention = ql.ModifiedFollowing
+dateGeneration = ql.DateGeneration.Backward
+endOfMonth = False
+schedule = ql.Schedule(issueDate,
+                     maturityDate,
+                     tenor,
+                     calendar,
+                     convention,
+                     convention,
+                     dateGeneration,
+                     endOfMonth)
+settlementDays = 1 # after trade when settlement
+faceAmount = 100
+coupon = [0.0175]
+dayCounter = ql.ActualActual() 
+
+#Fixed rate bond
+fixedRateBond = ql.FixedRateBond(settlementDays,
+                                 faceAmount,
+                                 schedule,
+                                 coupon,
+                                 dayCounter)
+
+# conduct pricing
+fixedRateBond.setPricingEngine(bondEngine)
+
+# print pricing result
+clean_price = fixedRateBond.cleanPrice() #clean_price => without coupon because of trade = derty price = cleanprice + coupon
+accrued_interest = fixedRateBond.accruedAmount()
+dirty_price = fixedRateBond.dirtyPrice()
+ytm = fixedRateBond.bondYield(dayCounter, ql.Compounded, ql.Semiannual)
+
+print("clean price = {}".format(clean_price))
+print("accrued interest = {}".format(accrued_interest))
+print('dirty price = {}'.format(dirty_price))
+print("Yield to maturiry ={}".format(ytm))
